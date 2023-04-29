32aa882287dfbc10ea4231c75c323305891610e0
junhyukPark <junni802@naver.com>
2023-04-29 16:40:27 +0900

    예외처리 추가

--- src/main/java/hello/itemservice/web/validation/ItemValidatior.java
+++ src/main/java/hello/itemservice/web/validation/ItemValidatior.java
@@ -19,17 +19,22 @@ public class ItemValidatior implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         Item item = (Item) target;
+        // 상품명에 대한 오류 처리를 위한 조건문
         if (!StringUtils.hasText(item.getItemName())) {
             errors.rejectValue("itemName", "required");
             ValidationUtils.rejectIfEmpty(errors, "itemName", "required");
         }
+        // 상품가격에 대한 오류 처리를 위한 조건문
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() >
                 1000000) {
             errors.rejectValue("price", "range", new Object[]{1000, 100000}, null);
         }
+
+        // 상품수량에 대한 오류 처리를 위한 조건문
         if (item.getQuantity() == null || item.getQuantity() > 10000) {
             errors.rejectValue("quantity", "max", new Object[]{9999}, null);
         }
+
         //특정 필드 예외가 아닌 전체 예외
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
